179e154087264afb07b23333aa037a6fefa78702
hoang2692 <hoang2692@gmail.com>
2020-07-30 15:56:14 +0700

    checkout

---
+++ src/Containers/PageServices/asset/index.js
@@ -0,0 +1,12 @@
+const img ={
+    imgComputer: require('./images/xuat file2-45.png'),
+    imgCocaRed: require('./images/cocain.PNG'),
+    imgCocaGray: require('./images/cola.png'),
+    imgSuccess: require('./images/xuat file2-38.png'),
+    imgCard1: require('./images/xuat file2-39.png'),
+    imgCard2: require('./images/hoanggeiiiiii.png'),
+    imgCard3: require('./images/hoang_buscu.png'),
+    imgDiagram: require('./images/hoangngugeogeingu.png'),
+}
+
+export default img;
