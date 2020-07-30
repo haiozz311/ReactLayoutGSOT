6d5372f5f905108dbc3f4edc3a235ddc0e07e3dd
hoang2692 <hoang2692@gmail.com>
2020-07-30 14:32:52 +0700

    pull

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Containers/Header';
 import Footer from './Containers/Footer/index';
+import Introduce from './Containers/PageIntroduce';
 
 function App() {
   return (
     <div>
       <Header />
+      <Introduce />
       <Footer />
     </div>
   );
