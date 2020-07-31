286ef4e4fddec321a4fe363d888ea12d561174aa
phuong260599 <nguyenphuong26051999@gmail.com>
2020-07-31 08:35:47 +0700

    pull

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Containers/Header';
 import Footer from './Containers/Footer/index';
+import Page5 from './Containers/Page_5';
 
 function App() {
   return (
     <div>
       <Header />
+      <Page5 />
       <Footer />
     </div>
   );
