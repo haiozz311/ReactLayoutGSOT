74f7cd69bb804704393215061b031d4e7914910b
hoang2692 <hoang2692@gmail.com>
2020-07-31 09:20:37 +0700

    services2

--- src/Routers.js
+++ src/Routers.js
@@ -4,6 +4,7 @@ import Header from './Containers/Header';
 import Footer from './Containers/Footer/index';
 import Introduce from './Containers/PageIntroduce';
 import Services from './Containers/PageServices';
+import Services2 from './Containers/PageServices2';
 
 class Routes extends React.Component {
 
@@ -14,6 +15,7 @@ class Routes extends React.Component {
                 <Switch>
                     <Route path='/' exact component={Introduce}/>
                     <Route path='/dichvu' exact component={Services}/>
+                    <Route path='/dichvu2' exact component={Services2}/>
                 </Switch>
                 <Footer />
             </div>
